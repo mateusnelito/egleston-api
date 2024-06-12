b665607c105bdad883ba42a0e818f325a16a30de
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-06-12 16:02:09 +0100

    feat: add a global error handler as plugin

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,33 @@
 import fastify from 'fastify';
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod';
+import routes from './routes'; // Import all registered routes
+import HttpStatusCodes from './utils/HttpStatusCodes';
+import errorHandlerPlugin from './plugins/errorHandler';
 
 // Instantiate the server
 const server = fastify();
+server.register(errorHandlerPlugin);
+
+// Add zod-type-provider validator compiler and serializer compiler
+// as default schema validator and serializer
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
 
 // Route to test the server status (health)
 server.get('/healthcheck', async (req, res) => {
   res.send({
     message: 'API Working...',
   });
+});
+
+server.register(routes); // Register all the routes to API
+
+// Define the 404 route
+server.setNotFoundHandler((request, reply) => {
+  reply.code(HttpStatusCodes.NOT_FOUND).send({ message: 'Route not found' });
 });
 
 const SERVER_PORT = Number(process.env.SERVER_PORT || 8000);
