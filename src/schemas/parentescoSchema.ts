f5149b433685775d7fe35976557bfcd062cc9807
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-06-24 12:54:36 +0100

    refactor: refactor parentesco schema

--- src/schemas/parentescoSchema.ts
+++ src/schemas/parentescoSchema.ts
@@ -1,31 +1,50 @@
 import { z } from 'zod';
-import { notFoundRequestSchema, simpleBadRequestSchema } from './globalSchema';
+import {
+  getAllResourcesParamsSchema,
+  notFoundRequestSchema,
+  simpleBadRequestSchema,
+} from './globalSchema';
+
+const parentescoBodySchema = z.object({
+  nome: z
+    .string({
+      required_error: 'O nome de parentesco é obrigatório.',
+      invalid_type_error: 'O nome de parentesco deve ser uma string.',
+    })
+    .trim()
+    .min(2, {
+      message: 'O nome de parentesco deve ter pelo menos 2 caracteres.',
+    })
+    .max(30, {
+      message: 'O nome de parentesco deve ter no máximo 30 caracteres.',
+    })
+    .regex(/^[a-zA-ZÀ-ÿ]+(?:\s[a-zA-ZÀ-ÿ]+)*$/, {
+      message:
+        'O nome de parentesco deve conter apenas letras e espaços e não pode começar ou terminar com espaço.',
+    }),
+});
+
+const parentescoParamsSchema = z.object({
+  parentescoId: z.coerce
+    .number({
+      required_error: 'O id de parentesco é obrigatório.',
+      invalid_type_error: 'O id de parentesco deve ser número.',
+    })
+    .int({ message: 'O id de parentesco deve ser inteiro.' })
+    .positive({ message: 'O id de parentesco deve ser positivo.' }),
+});
+
+const parentescoOkResponseSchema = z.object({
+  id: z.number().int().positive(),
+  nome: z.string(),
+});
+
 export const createParentescoSchema = {
   summary: 'Adiciona um novo parentesco',
   tags: ['parentescos'],
-  body: z.object({
-    nome: z
-      .string({
-        required_error: 'O nome de parentesco é obrigatório.',
-        invalid_type_error: 'O nome de parentesco deve ser uma string.',
-      })
-      .trim()
-      .min(2, {
-        message: 'O nome de parentesco deve ter pelo menos 2 caracteres.',
-      })
-      .max(30, {
-        message: 'O nome de parentesco deve ter no máximo 30 caracteres.',
-      })
-      .regex(/^[a-zA-ZÀ-ÿ]+(?:\s[a-zA-ZÀ-ÿ]+)*$/, {
-        message:
-          'O nome de parentesco deve conter apenas letras e espaços e não pode começar ou terminar com espaço.',
-      }),
-  }),
+  body: parentescoBodySchema,
   response: {
-    201: z.object({
-      id: z.number().int().positive(),
-      nome: z.string(),
-    }),
+    201: parentescoOkResponseSchema,
     400: simpleBadRequestSchema,
   },
 };
@@ -33,37 +52,10 @@ export const createParentescoSchema = {
 export const updateParentescoSchema = {
   summary: 'Atualiza um parentesco existente',
   tags: ['parentescos'],
-  params: z.object({
-    parentescoId: z.coerce
-      .number({
-        required_error: 'O id de parentesco é obrigatório.',
-        invalid_type_error: 'O id de parentesco deve ser número.',
-      })
-      .int({ message: 'O id de parentesco deve ser inteiro.' })
-      .positive({ message: 'O id de parentesco deve ser positivo.' }),
-  }),
-  body: z.object({
-    nome: z
-      .string({
-        required_error: 'O nome de parentesco é obrigatório.',
-        invalid_type_error: 'O nome de parentesco deve ser uma string.',
-      })
-      .trim()
-      .min(2, {
-        message: 'O nome de parentesco deve ter pelo menos 2 caracteres.',
-      })
-      .max(30, {
-        message: 'O nome de parentesco deve ter no máximo 30 caracteres.',
-      })
-      .regex(/^[a-zA-ZÀ-ÿ]+(?:\s[a-zA-ZÀ-ÿ]+)*$/, {
-        message:
-          'O nome de parentesco deve conter apenas letras e espaços e não pode começar ou terminar com espaço.',
-      }),
-  }),
+  params: parentescoParamsSchema,
+  body: parentescoBodySchema,
   response: {
-    200: z.object({
-      nome: z.string(),
-    }),
+    200: parentescoOkResponseSchema.omit({ id: true }),
     400: simpleBadRequestSchema,
     404: notFoundRequestSchema,
   },
@@ -72,31 +64,10 @@ export const updateParentescoSchema = {
 export const getParentescosSchema = {
   summary: 'Retorna todos os parentescos',
   tags: ['parentescos'],
-  querystring: z.object({
-    page_size: z.coerce
-      .number({
-        invalid_type_error: 'O tamanho da página deve ser número.',
-      })
-      .int({ message: 'O tamanho da página deve ser inteiro.' })
-      .positive({ message: 'O tamanho da página deve ser positivo.' })
-      .default(10),
-    cursor: z.coerce
-      .number({
-        invalid_type_error: 'O cursor deve ser número.',
-      })
-      .int({ message: 'O cursor deve ser inteiro.' })
-      .positive({ message: 'O cursor deve ser positivo.' })
-      .min(2, { message: 'O valor do cursor dever ser no minimo 2.' })
-      .nullish(),
-  }),
+  querystring: getAllResourcesParamsSchema,
   response: {
     200: z.object({
-      data: z.array(
-        z.object({
-          id: z.number().int().positive(),
-          nome: z.string(),
-        })
-      ),
+      data: z.array(parentescoOkResponseSchema),
       next_cursor: z.number().optional(),
     }),
   },
@@ -105,20 +76,9 @@ export const getParentescosSchema = {
 export const getParentescoSchema = {
   summary: 'Busca parentesco pelo id',
   tags: ['parentescos'],
-  params: z.object({
-    parentescoId: z.coerce
-      .number({
-        required_error: 'O id de parentesco é obrigatório.',
-        invalid_type_error: 'O id de parentesco deve ser número.',
-      })
-      .int({ message: 'O id de parentesco deve ser inteiro.' })
-      .positive({ message: 'O id de parentesco deve ser positivo.' }),
-  }),
+  params: parentescoParamsSchema,
   response: {
-    200: z.object({
-      id: z.number().int().positive(),
-      nome: z.string(),
-    }),
+    200: parentescoOkResponseSchema,
     404: notFoundRequestSchema,
   },
 };
