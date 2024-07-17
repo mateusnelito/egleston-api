e7043e66bb4e5b3424c5ca4f24a3aacec359b5ae
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-07-17 10:49:13 +0100

    feat: connect the anoLectivo controller and anoLectivo routes

--- src/routes/anoLectivoRoutes.ts
+++ src/routes/anoLectivoRoutes.ts
@@ -1,12 +1,13 @@
 import { FastifyInstance, FastifyPluginAsync } from 'fastify';
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
 import { postAnoLectivoSchema } from '../schemas/anoLectivoSchema';
+import { createAnoLectivo } from '../controllers/anoLectivoController';
 
 export const anoLectivoRoutes: FastifyPluginAsync = async (
   server: FastifyInstance
 ) => {
   server.withTypeProvider<ZodTypeProvider>().post('/', {
     schema: postAnoLectivoSchema,
-    handler: async () => {},
+    handler: createAnoLectivo,
   });
 };
