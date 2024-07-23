18fe03a7d263e0e2c0b1799753f64825e16ff649
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-07-23 12:27:08 +0100

    feat: add turnos routes structure

---
+++ src/routes/turnoRoutes.ts
@@ -0,0 +1,31 @@
+import { FastifyInstance, FastifyPluginAsync } from 'fastify';
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { postTurnoSchema } from '../schemas/turnoSchemas';
+
+export const turnoRoutes: FastifyPluginAsync = async (
+  server: FastifyInstance
+) => {
+  // POST
+  server.withTypeProvider<ZodTypeProvider>().post('/', {
+    schema: postTurnoSchema,
+    handler: () => {},
+  });
+
+  // PUT
+  server.withTypeProvider<ZodTypeProvider>().put('/:turnoId', {
+    schema: {},
+    handler: () => {},
+  });
+
+  // GET Unique
+  server.withTypeProvider<ZodTypeProvider>().get('/:turnoId', {
+    schema: {},
+    handler: () => {},
+  });
+
+  // GET all
+  server.withTypeProvider<ZodTypeProvider>().get('/', {
+    schema: {},
+    handler: () => {},
+  });
+};
