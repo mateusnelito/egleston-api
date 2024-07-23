564cb38c7391747edddcb68b4b08c889fb5a1ce4
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-07-23 10:42:30 +0100

    feat: add getSalaId service and update getSalaNome

--- src/services/salaServices.ts
+++ src/services/salaServices.ts
@@ -1,7 +1,7 @@
 import { prisma } from '../lib/prisma';
 import { postSalaBodyType } from '../schemas/salaSchemas';
 
-export async function getSalaNome(nome: string) {
+export async function getSalaByNome(nome: string) {
   return await prisma.sala.findUnique({
     where: { nome },
     select: { id: true },
@@ -10,4 +10,8 @@ export async function getSalaNome(nome: string) {
 
 export async function saveSala(data: postSalaBodyType) {
   return await prisma.sala.create({ data });
+}
+
+export async function getSalaId(id: number) {
+  return await prisma.sala.findUnique({ where: { id }, select: { id: true } });
 }
