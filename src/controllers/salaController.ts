08bec9c647dca5600605557516aec34608a04af8
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-07-23 10:53:33 +0100

    feat: add controller to handle update salas

--- src/controllers/salaController.ts
+++ src/controllers/salaController.ts
@@ -1,8 +1,14 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
-import { postSalaBodyType } from '../schemas/salaSchemas';
-import { getSalaNome, saveSala } from '../services/salaServices';
+import { postSalaBodyType, salaParamsType } from '../schemas/salaSchemas';
+import {
+  changeSala,
+  getSalaByNome,
+  getSalaId,
+  saveSala,
+} from '../services/salaServices';
 import BadRequest from '../utils/BadRequest';
 import HttpStatusCodes from '../utils/HttpStatusCodes';
+import NotFoundRequest from '../utils/NotFoundRequest';
 
 function throwNomeAlreadyExist() {
   throw new BadRequest({
@@ -12,14 +18,40 @@ function throwNomeAlreadyExist() {
   });
 }
 
+function throwNotFoundSala() {
+  throw new NotFoundRequest({
+    statusCode: HttpStatusCodes.NOT_FOUND,
+    message: 'ID da sala não existe.',
+  });
+}
+
 export async function createSalaController(
   request: FastifyRequest<{ Body: postSalaBodyType }>,
   reply: FastifyReply
 ) {
   const { nome } = request.body;
-  const isSalaNome = await getSalaNome(nome);
+  const isSalaNome = await getSalaByNome(nome);
 
   if (isSalaNome) throwNomeAlreadyExist();
   const sala = await saveSala(request.body);
   return reply.status(HttpStatusCodes.CREATED).send(sala);
+}
+
+export async function updateSalaController(
+  request: FastifyRequest<{ Params: salaParamsType; Body: postSalaBodyType }>,
+  reply: FastifyReply
+) {
+  const { salaId } = request.params;
+  const { nome } = request.body;
+
+  const [isSala, sala] = await Promise.all([
+    await getSalaId(salaId),
+    await getSalaByNome(nome),
+  ]);
+
+  if (!isSala) throwNotFoundSala();
+  if (sala && sala.id !== salaId) throwNomeAlreadyExist();
+
+  const updatedSala = await changeSala(salaId, request.body);
+  return reply.send(updatedSala);
 }
