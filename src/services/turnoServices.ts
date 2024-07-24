8e8a3313b3e283dcc34f7a9745150bd056a94a0a
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-07-24 14:59:04 +0100

    feat: add service to create turno and get it by inicio and termino

--- src/services/turnoServices.ts
+++ src/services/turnoServices.ts
@@ -1,8 +1,30 @@
 import { prisma } from '../lib/prisma';
+import { postTurnoBodyType } from '../schemas/turnoSchemas';
 
 export async function getTurnoByNome(nome: string) {
   return await prisma.turno.findUnique({
     where: { nome },
+    select: { id: true },
+  });
+}
+
+export async function saveTurno(data: postTurnoBodyType) {
+  return await prisma.turno.create({ data });
+}
+
+export async function getTurnos() {
+  return await prisma.turno.findMany({
+    select: { id: true, nome: true },
+    orderBy: { id: 'desc' },
+  });
+}
+
+export async function getTurnoByInicioAndTermino(
+  inicio: string,
+  termino: string
+) {
+  return await prisma.turno.findUnique({
+    where: { inicio_termino: { inicio, termino } },
     select: { id: true },
   });
 }
