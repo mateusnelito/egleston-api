2dc1a943680f84cbabdba4c6b98adee6a8774be5
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-07-25 12:08:34 +0100

    feat: add a fn to return if an start date is after start end date

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,10 @@
+import dayjs from 'dayjs';
+
 // Formate date to yyyy-mm-dd
 export function formatDate(date: Date): String {
   return date.toISOString().slice(0, 10);
+}
+
+export function isDateBeginAfterDateEnd(begin: Date, end: Date): boolean {
+  return dayjs(begin).isAfter(dayjs(end));
 }
