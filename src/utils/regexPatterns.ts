2efaf5066400bf3c1af4ee9c8040f7cb2cf337f2
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-08-07 17:24:01 +0100

    refactor: rename the global regEx's

--- src/utils/regexPatterns.ts
+++ src/utils/regexPatterns.ts
@@ -1,6 +1,6 @@
-export const FULL_NAME_REGEX = /^[A-Za-zÀ-ÖØ-öø-ÿ\s'-]+$/;
-export const OUTROS_CONTACTOS_REGEX = /^[a-zA-ZÀ-ÿ0-9.,;:'"\-\(\)\s]{5,255}$/;
-export const NUMERO_BI_REGEX = /^\d{9}[A-Z]{2}\d{3}$/;
-export const CURSO_NOME_REGEX =
+export const fullNameRegEx = /^[A-Za-zÀ-ÖØ-öø-ÿ\s'-]+$/;
+export const outrosContactosRegEx = /^[a-zA-ZÀ-ÿ0-9.,;:'"\-\(\)\s]{5,255}$/;
+export const numeroBiRegEx = /^\d{9}[A-Z]{2}\d{3}$/;
+export const cursoNomeRegEx =
   /^[A-Za-zÀ-ÖØ-öø-ÿ][A-Za-zÀ-ÖØ-öø-ÿ0-9 '’\-]{2,49}$/;
-export const DESCRICAO_REGEX = /^[A-Za-zÀ-ÖØ-öø-ÿ0-9\s.,;'"-]{10,500}$/;
+export const descricaoRegEx = /^[A-Za-zÀ-ÖØ-öø-ÿ0-9\s.,;'"-]{10,500}$/;
