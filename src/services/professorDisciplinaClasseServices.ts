5528657d2a3fd27eda4c6b6b15a67961160844d2
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-09-16 17:05:40 +0100

    feat: add service to handle professorDisciplinaClasse entity

---
+++ src/services/professorDisciplinaClasseServices.ts
@@ -0,0 +1,26 @@
+import { Prisma } from '@prisma/client';
+import { prisma } from '../lib/prisma';
+
+export async function getProfessorDisciplinaClasseById(
+  professorId: number,
+  disciplinaId: number,
+  classeId: number,
+  turmaId: number
+) {
+  return await prisma.professorDisciplinaClasse.findUnique({
+    where: {
+      professorId_disciplinaId_classeId_turmaId: {
+        professorId,
+        disciplinaId,
+        classeId,
+        turmaId,
+      },
+    },
+  });
+}
+
+export async function createProfessorDisciplinaClasse(
+  data: Prisma.ProfessorDisciplinaClasseUncheckedCreateInput
+) {
+  return await prisma.professorDisciplinaClasse.create({ data });
+}
