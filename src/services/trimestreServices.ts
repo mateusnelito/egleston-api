deae83d72ba64ee2f89006f04850a1bc834813ac
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-09-16 09:34:54 +0100

    bugfix: change the order by to retrie last trimestre in ano-lectivo

--- src/services/trimestreServices.ts
+++ src/services/trimestreServices.ts
@@ -10,7 +10,7 @@ export async function getLastTrimestreAddedInAnoLectivo(anoLectivoId: number) {
   return await prisma.trimestre.findFirst({
     where: { anoLectivoId },
     select: { id: true, termino: true },
-    orderBy: { id: 'asc' },
+    orderBy: { id: 'desc' },
   });
 }
 
