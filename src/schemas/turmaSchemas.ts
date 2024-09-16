7d6afae90bdd21260d057540269861ad384fd4ec
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-09-16 14:49:01 +0100

    bugfix: changed error message

--- src/schemas/turmaSchemas.ts
+++ src/schemas/turmaSchemas.ts
@@ -3,23 +3,23 @@ import { simpleBadRequestSchema } from './globalSchema';
 export const turmaBodySchema = z.object({
   id: z
     .number({
-      required_error: 'O id do turno é obrigatório.',
-      invalid_type_error: 'O id do turno deve ser número.',
+      required_error: 'O id da turma é obrigatório.',
+      invalid_type_error: 'O id da turma deve ser número.',
     })
-    .int({ message: 'O id do turno deve ser inteiro.' })
-    .positive({ message: 'O id do turno deve ser positivo.' }),
+    .int({ message: 'O id da turma deve ser inteiro.' })
+    .positive({ message: 'O id da turma deve ser positivo.' }),
   // TODO: Add appropriated nome regex
   nome: z
     .string({
-      required_error: 'O nome do turno é obrigatório.',
-      invalid_type_error: 'O nome do turno deve ser string.',
+      required_error: 'O nome da turma é obrigatório.',
+      invalid_type_error: 'O nome da turma deve ser string.',
     })
     .trim()
     .min(1, {
-      message: 'O nome do turno deve possuir no mínimo 1 caracteres.',
+      message: 'O nome da turma deve possuir no mínimo 1 caracteres.',
     })
     .max(30, {
-      message: 'O nome do turno deve possuir no máximo 30 caracteres.',
+      message: 'O nome da turma deve possuir no máximo 30 caracteres.',
     }),
   classeId: z
     .number({
