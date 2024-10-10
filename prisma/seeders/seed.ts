7cba223a6605814a79e585f4b002b3b1f00e9c51
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-10-10 11:40:12 +0100

    feat: create DisciplinaCurso seed

--- prisma/seeders/seed.ts
+++ prisma/seeders/seed.ts
@@ -1,5 +1,6 @@
 import { prisma } from '../../src/lib/prisma';
 import { seedAnoLectivo } from './anoLectivoSeeder';
+import { seedCursoDisciplina } from './cursoDisciplinaSeeder';
 import { seedCurso } from './cursoSeeder';
 import { seedDisciplina } from './disciplinaSeeder';
 import { seedParentesco } from './parentescoSeeder';
@@ -12,29 +13,16 @@ export function generateRandomNumber(min: number, max: number) {
 }
 
 async function seed() {
-  seedAnoLectivo()
-    .then(() => console.log('\nSeed Ano Lectivo done!\n'))
-    .catch((err) => console.log(`Error seeding Ano Lectivo: \n${err}`));
+  await Promise.all([
+    seedAnoLectivo(),
+    seedTurno(),
+    seedSala(),
+    seedParentesco(),
+    seedCurso(),
+    seedDisciplina(),
+  ]);
 
-  seedTurno()
-    .then(() => console.log('\nSeed Turno done!\n'))
-    .catch((err) => console.log(`\nError seeding Turno: \n${err}`));
-
-  seedSala()
-    .then(() => console.log('\nSeed Sala done!\n'))
-    .catch((err) => console.log(`\nError seeding Sala: \n${err}`));
-
-  seedParentesco()
-    .then(() => console.log('\nSeed Parentesco done!\n'))
-    .catch((err) => console.log(`\nError seeding Parentesco: \n${err}`));
-
-  seedCurso()
-    .then(() => console.log('\nSeed Curso done!\n'))
-    .catch((err) => console.log(`\nError seeding Curso: \n${err}`));
-
-  seedDisciplina()
-    .then(() => console.log('\nSeed Disciplina done!\n'))
-    .catch((err) => console.log(`\nError seeding Disciplina: \n${err}`));
+  await seedCursoDisciplina();
 }
 
 seed()
