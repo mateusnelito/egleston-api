01d3ae3e2fc7d98c8ad4661efcd156227e02ea63
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-10-10 11:22:26 +0100

    bugfix: insert inicio and termino in time format

--- prisma/seeders/turnoSeeder.ts
+++ prisma/seeders/turnoSeeder.ts
@@ -3,9 +3,9 @@ import { prisma } from '../../src/lib/prisma';
 export async function seedTurno() {
   await prisma.turno.createMany({
     data: [
-      { nome: 'Manhã', inicio: '07:00', termino: '12:00' },
-      { nome: 'Tarde', inicio: '13:00', termino: '18:05' },
-      { nome: 'Noite', inicio: '18:30', termino: '22:00' },
+      { nome: 'Manhã', inicio: '07:00:00', termino: '12:00:00' },
+      { nome: 'Tarde', inicio: '13:00:00', termino: '18:05:00' },
+      { nome: 'Noite', inicio: '18:30:00', termino: '22:00:00' },
     ],
   });
 }
