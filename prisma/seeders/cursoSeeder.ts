9bd3365adb58a5a0e6749872cb6899178a0ca05d
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-10-10 09:43:47 +0100

    feat: extend the number of cursos

--- prisma/seeders/cursoSeeder.ts
+++ prisma/seeders/cursoSeeder.ts
@@ -4,34 +4,64 @@ export async function seedCurso() {
   await prisma.curso.createMany({
     data: [
       {
-        nome: 'Engenharia de Software',
-        descricao:
-          'Curso voltado para o desenvolvimento de software, incluindo engenharia de requisitos, arquitetura e design de software.',
-        duracao: 4,
-      },
-      {
-        nome: 'Administração',
-        descricao:
-          'Curso focado em gestão empresarial, planeamento estratégico e finanças.',
-        duracao: 4,
-      },
-      {
-        nome: 'Design Gráfico',
-        descricao:
-          'Curso destinado à criação de projetos visuais para mídias impressas e digitais.',
-        duracao: 3,
-      },
-      {
         nome: 'Medicina',
         descricao:
-          'Curso com foco no estudo do corpo humano e formação de médicos generalistas.',
+          'Curso destinado à formação de médicos, abrangendo conhecimentos em saúde, diagnóstico e tratamento de doenças.',
         duracao: 6,
       },
       {
-        nome: 'Direito',
+        nome: 'Enfermagem',
         descricao:
-          'Curso voltado ao estudo das ciências jurídicas, preparando os alunos para diversas áreas do direito.',
-        duracao: 5,
+          'Formação de profissionais de enfermagem, com foco em cuidados de saúde e assistência ao paciente.',
+        duracao: 4,
+      },
+      {
+        nome: 'Análises Clínicas',
+        descricao:
+          'Curso voltado para a formação de técnicos em análises laboratoriais, focando na coleta e interpretação de exames.',
+        duracao: 3,
+      },
+      {
+        nome: 'Informática',
+        descricao:
+          'Curso que aborda fundamentos da computação, programação, e aplicações de software.',
+        duracao: 3,
+      },
+      {
+        nome: 'Eletrónica',
+        descricao:
+          'Formação em eletrónica, incluindo circuitos, sistemas digitais e manutenção de equipamentos eletrónicos.',
+        duracao: 3,
+      },
+      {
+        nome: 'Mecânica',
+        descricao:
+          'Curso voltado para a formação de técnicos em mecânica, com foco em manutenção e reparo de veículos e máquinas.',
+        duracao: 3,
+      },
+      {
+        nome: 'PUNIV',
+        descricao:
+          'Curso de Formação Universitária com foco em várias áreas do conhecimento, visando a qualificação profissional.',
+        duracao: 3,
+      },
+      {
+        nome: 'Gestão de Recursos Humanos (GRH)',
+        descricao:
+          'Formação em gestão de pessoas, abordando recrutamento, seleção, e desenvolvimento organizacional.',
+        duracao: 3,
+      },
+      {
+        nome: 'Gestão de Empresas',
+        descricao:
+          'Curso que capacita estudantes em administração, finanças e marketing para a gestão eficaz de empresas.',
+        duracao: 3,
+      },
+      {
+        nome: 'Agronomia',
+        descricao:
+          'Curso voltado para a formação de agrónomos, com foco em práticas agrícolas sustentáveis e desenvolvimento rural.',
+        duracao: 4,
       },
     ],
   });
