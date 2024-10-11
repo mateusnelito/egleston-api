ea346fef4ba34ddf9519ba5b998cbb4aecd48109
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-10-11 12:18:29 +0100

    feat: envelope with try...cath

--- prisma/seeders/alunoSeeder.ts
+++ prisma/seeders/alunoSeeder.ts
@@ -4,26 +4,30 @@ import path from 'path';
 import { prisma } from '../../src/lib/prisma';
 
 export async function seedAluno() {
-  const dataPath = path.join(__dirname, 'data');
+  try {
+    const dataPath = path.join(__dirname, 'data');
 
-  const alunosData: Prisma.AlunoUncheckedCreateInput[] = JSON.parse(
-    fs.readFileSync(path.join(dataPath, 'alunos.json'), 'utf-8')
-  );
-
-  const alunosEnderecosData: Prisma.AlunoEnderecoUncheckedCreateInput =
-    JSON.parse(
-      fs.readFileSync(path.join(dataPath, 'alunosEnderecos.json'), 'utf8')
+    const alunosData: Prisma.AlunoUncheckedCreateInput[] = JSON.parse(
+      fs.readFileSync(path.join(dataPath, 'alunos.json'), 'utf-8')
     );
 
-  const alunosContactosData: Prisma.AlunoContactoUncheckedCreateInput =
-    JSON.parse(
-      fs.readFileSync(path.join(dataPath, 'alunosContactos.json'), 'utf8')
-    );
+    const alunosEnderecosData: Prisma.AlunoEnderecoUncheckedCreateInput =
+      JSON.parse(
+        fs.readFileSync(path.join(dataPath, 'alunosEnderecos.json'), 'utf8')
+      );
 
-  await prisma.aluno.createMany({ data: alunosData });
+    const alunosContactosData: Prisma.AlunoContactoUncheckedCreateInput =
+      JSON.parse(
+        fs.readFileSync(path.join(dataPath, 'alunosContactos.json'), 'utf8')
+      );
 
-  await Promise.all([
-    prisma.alunoEndereco.createMany({ data: alunosEnderecosData }),
-    prisma.alunoContacto.createMany({ data: alunosContactosData }),
-  ]);
+    await prisma.aluno.createMany({ data: alunosData });
+
+    await Promise.all([
+      prisma.alunoEndereco.createMany({ data: alunosEnderecosData }),
+      prisma.alunoContacto.createMany({ data: alunosContactosData }),
+    ]);
+  } catch (err) {
+    throw new Error(`\nError seeding Aluno \n${err}`);
+  }
 }
--- prisma/seeders/responsavelSeeder.ts
+++ prisma/seeders/responsavelSeeder.ts
@@ -4,26 +4,41 @@ import path from 'path';
 import { prisma } from '../../src/lib/prisma';
 
 export async function seedResponsavel() {
-  const dataPath = path.join(__dirname, 'data');
+  try {
+    const dataPath = path.join(__dirname, 'data');
 
-  const responsaveisData: Prisma.ResponsavelUncheckedCreateInput[] = JSON.parse(
-    fs.readFileSync(path.join(dataPath, 'responsaveis.json'), 'utf-8')
-  );
+    const responsaveisData: Prisma.ResponsavelUncheckedCreateInput[] =
+      JSON.parse(
+        fs.readFileSync(path.join(dataPath, 'responsaveis.json'), 'utf-8')
+      );
 
-  const responsaveisEnderecosData: Prisma.ResponsavelEnderecoUncheckedCreateInput =
-    JSON.parse(
-      fs.readFileSync(path.join(dataPath, 'responsaveisEnderecos.json'), 'utf8')
-    );
+    const responsaveisEnderecosData: Prisma.ResponsavelEnderecoUncheckedCreateInput =
+      JSON.parse(
+        fs.readFileSync(
+          path.join(dataPath, 'responsaveisEnderecos.json'),
+          'utf8'
+        )
+      );
 
-  const responsaveisContactosData: Prisma.ResponsavelContactoUncheckedCreateInput =
-    JSON.parse(
-      fs.readFileSync(path.join(dataPath, 'responsaveisContactos.json'), 'utf8')
-    );
+    const responsaveisContactosData: Prisma.ResponsavelContactoUncheckedCreateInput =
+      JSON.parse(
+        fs.readFileSync(
+          path.join(dataPath, 'responsaveisContactos.json'),
+          'utf8'
+        )
+      );
 
-  await prisma.responsavel.createMany({ data: responsaveisData });
+    await prisma.responsavel.createMany({ data: responsaveisData });
 
-  await Promise.all([
-    prisma.responsavelEndereco.createMany({ data: responsaveisEnderecosData }),
-    prisma.responsavelContacto.createMany({ data: responsaveisContactosData }),
-  ]);
+    await Promise.all([
+      prisma.responsavelEndereco.createMany({
+        data: responsaveisEnderecosData,
+      }),
+      prisma.responsavelContacto.createMany({
+        data: responsaveisContactosData,
+      }),
+    ]);
+  } catch (err) {
+    throw new Error(`\nError seeding Responsavel \n${err}`);
+  }
 }
