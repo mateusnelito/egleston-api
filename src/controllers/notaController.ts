4777b3d327c6d2c969228e1c503a4482a0f64036
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-10-12 08:23:33 +0100

    feat: add create nota endpoint

--- src/controllers/notaController.ts
+++ src/controllers/notaController.ts
@@ -1,6 +1,37 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
-import { getAlunosWithoutNotaQueryStringDataType } from '../schemas/notaSchema';
+import {
+  getAlunosWithoutNotaQueryStringDataType,
+  notaDataType,
+} from '../schemas/notaSchema';
 import { getAlunosWithoutNotas } from '../services/alunoServices';
+import {
+  createNota,
+  getNotaById,
+  validateNotaData,
+} from '../services/notaServices';
+import { throwDuplicatedNotaError } from '../utils/controllers/notaControllerUtil';
+import HttpStatusCodes from '../utils/HttpStatusCodes';
+
+export async function createNotaController(
+  request: FastifyRequest<{ Body: notaDataType }>,
+  reply: FastifyReply
+) {
+  const { alunoId, classeId, disciplinaId, trimestreId } = request.body;
+
+  await validateNotaData({ alunoId, classeId, disciplinaId, trimestreId });
+  const nota = await getNotaById({
+    alunoId,
+    classeId,
+    disciplinaId,
+    trimestreId,
+  });
+
+  if (nota) throwDuplicatedNotaError();
+
+  return reply
+    .status(HttpStatusCodes.CREATED)
+    .send(await createNota(request.body));
+}
 
 export async function getAlunosWithoutNotaController(
   request: FastifyRequest<{
--- src/routes/notaRoutes.ts
+++ src/routes/notaRoutes.ts
@@ -1,9 +1,21 @@
 import { FastifyInstance, FastifyPluginAsync } from 'fastify';
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
-import { getAlunosWithoutNotaSchema } from '../schemas/notaSchema';
-import { getAlunosWithoutNotaController } from '../controllers/notaController';
+import {
+  createNotaController,
+  getAlunosWithoutNotaController,
+} from '../controllers/notaController';
+import {
+  createNotaSchema,
+  getAlunosWithoutNotaSchema,
+} from '../schemas/notaSchema';
 
 const notaRoutes: FastifyPluginAsync = async (server: FastifyInstance) => {
+  // POST
+  server.withTypeProvider<ZodTypeProvider>().post('/', {
+    schema: createNotaSchema,
+    handler: createNotaController,
+  });
+
   // Get all Alunos Without Notas
   server.withTypeProvider<ZodTypeProvider>().get('/alunos/sem-notas', {
     schema: getAlunosWithoutNotaSchema,
